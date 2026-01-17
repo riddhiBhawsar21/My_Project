b3b356d9e2fe6d20a10004c0d7627d77a8ee15b0
riddhiBhawsar21 <104494863+riddhiBhawsar21@users.noreply.github.com>
2026-01-17 17:14:12 +0530

    Add files via upload

---
+++ App (5).jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Products from './Products'
+import CartItem from './CartItem'
+
+
+const App = () => {
+  return (
+   <>
+   <Products>
+
+   </Products> 
+   
+   <CartItem></CartItem>
+  
+   </>
+  )
+}
+
+export default App
